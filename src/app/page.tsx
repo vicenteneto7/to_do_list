2f7ba0bf610a4c5184f33f19d3b26fe4f661746b
Vicente <pvicenteneto7@gmail.com>
2023-09-10 10:43:17 -0300

    astyle: estilização do input e div das tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,59 +18,56 @@ export default function Home() {
     setTask(newList);
   };
 
-  const HandleToggle = ( id : string) => {
-    
-    const newList = tasks.map(task => (
+  const HandleToggle = (id: string) => {
+    const newList = tasks.map((task) =>
       task.id == id ? { ...task, finished: !task.finished } : task
-    ))
-    setTask(newList)
+    );
+    setTask(newList);
   };
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="w-[33.813rem] h-[31.125rem] rounded-[5px] bg-slate-400">
-        <div className="flex justify-evenly py-8">
-          <input
-            onChange={(e) => setInput(e.currentTarget.value)}
-            type="text"
-            className="pl-3 w-[21.375rem] h-10 outline-none border-gray-400"
-            placeholder="O que tenho que fazer..."
-          />
-          <button
-            onClick={addNewTask}
-            className="w-32 h-10 rounded bg-purple-600 text-white text-base cursor-pointer"
+    <div className="w-full h-full flex justify-center">
+      <div className="flex justify-evenly w-[36.125rem] h-14 mt-10">
+        <input
+          id="input"
+          onChange={(e) => setInput(e.currentTarget.value)}
+          type="text"
+          className="pl-9 w-[36.125rem] outline-none h-14 bg-slate-700 shadow-3xl rounded-[16px]"
+          placeholder="O que tenho que fazer..."
+        />
+        <button onClick={addNewTask} className="cursor-pointer flex items-center relative right-8">
+          <Image src="/images/arrow.png" height={17} width={17} alt="" unoptimized />
+        </button>
+        
+      </div>
+      <div className="flex flex-col items-center justify-center px-5 mt-7 gap-3">
+        {tasks.map((task) => (
+          <div
+            className="flex items-center flex-row justify-between w-[28.063rem] h-14 px-6 bg-slate-700 shadow-4xl rounded-[16px] drop-shadow-filter"
+            style={{ background: task.finished ? "#f6ffd2" : "rgb(51 65 85)", fill: '#38404B'}}
+            key={task.id}
           >
-            Adicionar
-          </button>
-        </div>
-        <div className="flex flex-col items-center justify-center px-5 mt-7 gap-3">
-          {tasks.map((task) => (
-            <div
-              id="div-bg"
-              className="flex flex-row justify-between w-[31rem] h-[3.75rem] py-[1.30rem] px-5"
-              style={{background: task.finished ? '#e8ff8b' : '#7e7979'}}
-              key={task.id}
-            >
-              <Image
-                onClick={() => HandleToggle(task.id)}
-                src="/images/Vector-1.png"
-                height={13}
-                width={13}
-                alt="finished"
-                className="cursor-pointer"
-              />
-              <p>{task.task}</p>
-              <Image
-                onClick={() => deleteTask(task.id)}
-                src="/images/trash-red.png"
-                height={13}
-                width={13}
-                alt="trash"
-                className="cursor-pointer"
-              />
-            </div>
-          ))}
-        </div>
+            <Image
+              onClick={() => HandleToggle(task.id)}
+              src="/images/concluded.png"
+              height={12}
+              width={15}
+              alt="finished"
+              className="cursor-pointer"
+              unoptimized
+            />
+            <p className="flex flex-rap">{task.task}</p>
+            <Image
+              onClick={() => deleteTask(task.id)}
+              src="/images/trash.png"
+              height={11}
+              width={15}
+              alt="trash"
+              className="cursor-pointer"
+              unoptimized
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'image': "url('/public/background.png')",
+      
+      boxShadow: {
+        '3xl': '3px 3px 8px 0px rgba(46, 53, 62, 0.90) inset, -3px -3px 6px 0px rgba(66, 75, 88, 0.90) inset, 3px -3px 6px 0px rgba(46, 53, 62, 0.20) inset, -3px 3px 6px 0px rgba(46, 53, 62, 0.20) inset, -1px -1px 2px 0px rgba(46, 53, 62, 0.50), 1px 1px 2px 0px rgba(66, 75, 88, 0.30)', 
+        '4xl': '-1px -1px 2px 0px rgba(48, 54, 64, 0.50) inset, 1px 1px 2px 0px rgba(64, 74, 86, 0.30) inset'     
+      },
+      dropShadow: {
+        'filter': ['drop-shadow(5px 5px 13px rgba(48, 54, 64, 0.90)) drop-shadow(-5px -5px 10px rgba(64, 74, 86, 0.90)) drop-shadow(5px -5px 10px rgba(48, 54, 64, 0.20)) drop-shadow(-5px 5px 10px rgba(48, 54, 64, 0.20))']
       }
     },
   },
